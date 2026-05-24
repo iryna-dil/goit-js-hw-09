aaaa1054183bf69d7bd899fae0538b7cf0e59edc
iryna-dil <iradil372@gmail.com>
2026-05-24 12:47:13 +0000

    Deploying to gh-pages from @ iryna-dil/goit-js-hw-09@b4f222fe0c747f4886d75800b656bad49cf1ba3d 🚀

---
+++ 1-gallery.js
@@ -0,0 +1,8 @@
+import"./assets/styles-CdS4YV3W.js";import{S as p}from"./assets/vendor-CgTBfC_f.js";const e=[{preview:"https://cdn.pixabay.com/photo/2019/06/19/18/26/field-4285217_960_720.jpg",original:"https://cdn.pixabay.com/photo/2019/06/19/18/26/field-4285217_1280.jpg",description:"Hokkaido Flower"},{preview:"https://cdn.pixabay.com/photo/2019/11/23/11/33/landscape-4645024_960_720.jpg",original:"https://cdn.pixabay.com/photo/2019/11/23/11/33/landscape-4645024_1280.jpg",description:"Container Haulage Freight"},{preview:"https://cdn.pixabay.com/photo/2019/12/14/18/40/sunrise-4695484_960_720.jpg",original:"https://cdn.pixabay.com/photo/2019/12/14/18/40/sunrise-4695484_1280.jpg",description:"Aerial Beach View"},{preview:"https://cdn.pixabay.com/photo/2019/05/14/16/43/running-4202820_960_720.jpg",original:"https://cdn.pixabay.com/photo/2019/05/14/16/43/running-4202820_1280.jpg",description:"Shoes"},{preview:"https://cdn.pixabay.com/photo/2019/11/08/23/11/river-4613258_960_720.jpg",original:"https://cdn.pixabay.com/photo/2019/11/08/23/11/river-4613258_1280.jpg",description:"River"},{preview:"https://cdn.pixabay.com/photo/2019/06/16/19/03/lavenders-4278874_960_720.jpg",original:"https://cdn.pixabay.com/photo/2019/06/16/19/03/lavenders-4278874_1280.jpg",description:"Lavender"}],t=document.querySelector(".gallery"),n=e.map(({preview:i,original:o,description:a})=>`
+			<li class="gallery-item">
+				<a class="gallery-link" href="${o}">
+					<img class="gallery-image" src="${i}" alt="${a}" />
+				</a>
+			</li>
+		`).join("");t.insertAdjacentHTML("beforeend",n);new p(".gallery .gallery-link",{captionsData:"alt",captionPosition:"bottom",captionDelay:250});
+//# sourceMappingURL=1-gallery.js.map
